7239aad824a0b9e0a0982a10a2d7a843ad6362af
Damien LAINÉ <damien.laine@gmail.com>
2020-09-08 14:23:39 +0200

    Fix correct WebSockets parameters for traefik and MQTT

--- start.sh
+++ start.sh
@@ -67,11 +67,11 @@ fi
 LABELS=""
 if [[ "$LINTO_STACK_USE_SSL" == true ]]; then
     LABELS="${LABELS}, *ssl"
-    [[ "$LINTO_STACK_USE_ACME" == true ]] && LABELS="${LABELS}, *acme-wss"
+    [[ "$LINTO_STACK_USE_ACME" == true ]] && [[ "$LINTO_STACK_MQTT_OVER_WS" == true ]] && LABELS="${LABELS}, *acme-wss"
+    [[ "$LINTO_STACK_USE_ACME" == true ]] && [[ "$LINTO_STACK_MQTT_OVER_WS" == false ]] && LABELS="${LABELS}, *acme"
     [[ "$LINTO_STACK_MQTT_OVER_WS" == true ]] && LABELS="${LABELS}, *wss" # Secure Websocket support for MQTT
 else
-    [[ "$LINTO_STACK_USE_ACME" == true ]] && LABELS="${LABELS}, *acme"
-    [[ "$LINTO_STACK_MQTT_OVER_WS" == true ]] && LABELS="${LABELS}, *ws" # Non-secure websocket support for MQTT
+    [[ "$LINTO_STACK_MQTT_OVER_WS" == true ]] && [[ "$LINTO_STACK_MQTT_OVER_WS" == true ]] && LABELS="${LABELS}, *ws" # Non-secure websocket support for MQTT
 fi
 
 # Stack deployment using correct labels
