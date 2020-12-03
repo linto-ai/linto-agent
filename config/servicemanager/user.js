6fcccef6845dc27aa29cfcf65740137e0e10dee8
Ilyes Rebai <irebai@linagora.com>
2020-12-03 14:31:15 +0100

    fix environment variable's name: LINTO_STACK_STT_SERVICE_MANAGER_MONGODB_PSWD

--- config/servicemanager/user.js
+++ config/servicemanager/user.js
@@ -1,5 +1,5 @@
 db.createUser({
     user: "${LINTO_STACK_STT_SERVICE_MANAGER_MONGODB_USER}",
-    pwd: "${LINTO_STACK_STT_SERVICE_MANAGER_MOGODB_PSWD}",
+    pwd: "${LINTO_STACK_STT_SERVICE_MANAGER_MONGODB_PSWD}",
     roles: ["readWrite"]
 })
